0e46ea10441086fca0782b807b93162e9bdaee74
Svetashisha <148446483+Svetashisha@users.noreply.github.com>
2024-09-04 14:52:37 +0300

    Add files via upload

---
+++ алина света.py
@@ -0,0 +1,25 @@
+import math
+while True:
+    try:
+        a = int(input("ведите число a"))
+        b = int(input("ведите число b"))
+        c = int(input("ведите число c"))
+    except ValueError:
+            print("Неправильный тип данных")
+    else:
+        D = b**2 - 4*a*c
+        if D == 0:
+            x = -b/(2*a)
+            print (x)
+            break
+        elif D < 0:
+            print ("Корней нет")
+            break
+        elif D > 0:
+            x1 = (-b + math.sqrt(D)/2*a)
+            x2 = (-b - math.sqrt(D)/2*a)
+            print(x1,x2)
+            break
+                 
+                       
+            
